9db65a3e7963c51b460e7de6dc2c5e831180157a
Ahmed Elnasseh <elnassehag@vcu.edu>
2021-07-26 05:36:39 -0400

    second

--- src/App.js
+++ src/App.js
@@ -6,154 +6,136 @@ import Payment from './components/Payment'
 
 
 class App extends React.Component {
- 
+
 
   //data from sample.json will be represented by "records", remittance advice/reciept will be represented by "remAdvice" and
   //is the current selected customer, and "find" is the input used to locate the customer's records.
   state = {
-    records,
+    records: [],
     remAdvice: '',
-    receipt: []
+    // receipt: []
 
   }
 
 
   //sets the state to the correct invoice when Paymerang employees click on a customer
-  handleCustomerClick = (client, e) => {
+  handleCustomerClick = (advice, e) => {
     e.preventDefault();
     this.setState({
-      remAdvice: client,
-      
+      remAdvice: advice,
+
     })
-    console.log(client)
+    console.log(advice)
     console.log('Paymerang is awesome!')
   }
 
-  handleReceipt = (records, e) => {
-    e.preventDefault();
-    this.setState({
-      receipt: records
-    })
+  componentDidMount() {
+    console.log(records)
+    this.setState({ records })
   }
 
-  
+  // handleStateChange(advice, e){
+  //   if(e.event.target === {client}){
+  //     this.setState({
+  //       remAdvice: advice[client]
+  //     })
+  //   }
+  // }
+ 
+
 
   render() {
 
-  
+    const { records } = this.state
+    const advice = records.map(recordsIn => {
+      return (
+        <div key={recordsIn.Payee.Name}>
+          <h2>Payee</h2>
+        Name: {recordsIn.Payee.Name}<br />
+        Fax: {recordsIn.Payee.Fax}<br />
+        Phone: {recordsIn.Payee.Phone}<br />
+        Address: {recordsIn.Payee.Address.Address1}<br />
+        City: {recordsIn.Payee.Address.City}<br />
+        State or Province: {recordsIn.Payee.Address.StateOrProvince}<br />
+        Country: {recordsIn.Payee.Address.Country}<br />
+        PostalCode: {recordsIn.Payee.Address.PostalCode}<br />
+          <br />
+        Attention: {recordsIn.Payee.Attention}<br />
+        Submission Date: {recordsIn.Payee.SubmissionDate}<br />
+          <br />
+          <h2>Payment</h2>
+        PAN: {recordsIn.Payment.PAN}<br />
+        CVV: {recordsIn.Payment.CVV}<br />
+        Exp: {recordsIn.Payment.Exp}<br />
 
-    //map through the records of json to return payment info
-    const filterFind = this.state.records.map(
-      (payee) => {
-        console.log(payee.Payee.Name)
+          <h2>Remittance</h2>
+          <ul>
+            {recordsIn.Remittance.map(remit => {
+              return (
+                <ul>
+                  <li key={remit.PayorId}>Payor Name: {remit.PayorName}</li>
+                  <li>Payor Id: {remit.PayorId}</li>
+                  <li>Invoice No: {remit.InvoiceNo}</li>
+                  <li>Descriptiom: {remit.Description}</li>
+                  <li>Amount: {remit.Amount}</li>
+                  <br/>
+                </ul>
+                
+              )
+            })}
+          </ul>
 
-        return payee
+        </div>
+      )
+    })
 
-      }
 
-    )
     //map through the records to single out the Payee Name
-    const clients = filterFind.map(client => client.Payee.Name)
+    const clients = records.map(client => client.Payee.Name)
 
-    //sends the payment and customer data of the invoice if a customer is selected
-    const invoice = this.state.records.map((cli) => {
-      
-      <Payment
-        //customer information
-        customerName={cli.Payee.Name}
-        customerFax={cli.Payee.Fax}
-        customerPhone={cli.Payee.Phone}
-        customerAddress={cli.Payee.Address.Address1}
-        customerCity={cli.Payee.Address.City}
-        customerState={cli.Payee.Address.StateOrProvince}
-        customerPostal={cli.Payee.Address.PostalCode}
-        customerAttention={cli.Payee.Attention}
-        customerDate={cli.Payee.SubmissionDate}
-
-        //customer payments information
-        paycardPAN={cli.Payment.PAN}
-        paycardCVV={cli.Payment.CVV}
-        paycardExp={cli.Payment.Exp}
-
-        //customer remittance
-        remittance={cli.Remittance}
-      />
-      console.log(cli)
-      return cli
-      
-    })
-
-    const infoyc = filterFind.map((info) => {
-      return info.Payee
-    })
-
-    // let disp = ''
-    // if (this.state.remAdvice) {
-    //    disp = (
-    //     <div
-    //     //customer information
-    //     customerName={this.state.remAdvice.Payee.Name}
-    //     customerFax={this.state.remAdvice.Payee.Fax}
-    //     customerPhone={this.state.remAdvice.Payee.Phone}
-    //     customerAddress={this.state.remAdvice.Payee.Address.Address1}
-    //     customerCity={this.state.remAdvice.Payee.Address.City}
-    //     customerState={this.state.remAdvice.Payee.Address.StateOrProvince}
-    //     customerPostal={this.state.remAdvice.Payee.Address.PostalCode}
-    //     customerAttention={this.state.remAdvice.Payee.Attention}
-    //     customerDate={this.state.remAdvice.Payee.SubmissionDate}
-
-    //     //customer payments information
-    //     paycardPAN={this.state.remAdvice.Payment.PAN}
-    //     paycardCVV={this.state.remAdvice.Payment.CVV}
-    //     paycardExp={this.state.remAdvice.Payment.Exp}
-
-    //     //customer remittance
-    //     remittance={this.state.remAdvice.Remittance}
-       
-
-    // />
-    //    )
-     
-        
-    // }
-
-    console.log(filterFind)
+  
     console.log(clients)
-    console.log(this.state.remAdvice)
-    console.log(infoyc)
-    console.log(invoice)
+    // console.log(this.state.remAdvice)
+    console.log(advice)
 
 
-   
+    const filterClients = advice.filter(fc => {
+      console.log(advice)
+      return advice.client
+    })
 
+    
 
 
     return (
 
-      
+
       <div >
-        
+
         <header> <h1> Paymerang </h1> </header>
 
 
-        <div >{clients.map(client => <li key={client.toString()}><button onClick={(e) => this.handleCustomerClick(client, e)} >
+        <div >{clients.map(client => <li key={client.toString()}><button onClick={(e) => this.handleCustomerClick(advice, e)} >
           {client}
         </button></li>)}</div><br />
+        {/* <div>{<React.Fragment>{advice}</React.Fragment>}</div> */}
+        <div>{this.state.remAdvice}</div>
 
-        <p>{this.state.remAdvice}</p>   
+
 
         <div>
-          <Payment
+          {/* <Payment
             lion={this.state.remAdvice}
           tiger = {invoice}
+          bear={this.state.remAdvice}
                    
       
-          />
+          /> */}
         </div>
 
       </div>
-     
-     
+
+
     )
   }
 }
--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,54 +1,111 @@
-import React from 'react';
-import records from '../sample.json'
+// import React from 'react';
+// import ReactDOM from 'react-dom'
+// import records from '../sample.json'
 
-class Payment extends React.Component {
+// class Payment extends React.Component {
 
-    state = {
-        file: ''
-    }
-    handleBlack = (displayR, e) => {
-        e.preventDefault();
-        this.setState({
-            file: displayR
-        })
-        console.log(this.state.file)
-    }
+//     state = {
+//         records: []
+
+//     }
+
+//     componentDidMount() {
+//         console.log(records);
+//         this.setState({ records });
+//     }
+
+//     // handleBlack = (displayR, e) => {
+//     //     e.preventDefault();
+//     //     this.setState({
+//     //         file: displayR
+//     //     })
+//     //     console.log(this.state.file)
+//     // }
 
 
-    render() {
-        // const {customerName, customerFax, customerPhone, customerAddress, customerCity, customerState, customerPostal, customerAttention, customerDate, payCardPAN, payCardCVV, payCardExp, remittance} = this.props.lion
+//     render() {
+//         const { records } = this.state;
+//         const resume = records.map(recordsIn => {
+//             return (
+//                 <div key={recordsIn.Payee.Name}>
+//                     {recordsIn.Payee}
+//                     <ul>
+//                         {recordsIn.Remittance.map(remit => (
 
-        const displayR = records.map((r) => {
-            return r.Payment.PAN
-        })
+//                             <li key={remit.PayorId}>{remit.PayorName}</li>
+//                         ))}
+
+//                     </ul>
+//                 </div>
+
+//             )
+//         })
+
+
+//         return <div>{<React.Fragment>{resume}</React.Fragment>}</div>
+
+//         {/* // const {customerName, customerFax, customerPhone, customerAddress, customerCity, customerState, customerPostal, customerAttention, customerDate, payCardPAN, payCardCVV, payCardExp, remittance} = this.props.lion
+
+//         // const displayR = records.map((r) => {
+//             */}
+
+//         {/* //     return (
+//         //         <div>
+//         //         <h2>Payee</h2>    
+//         //         <p>Name: {r.Payee.Name}</p>
+//         //         <p>Fax: {r.Payee.Fax}</p>  
+//         //         <p>Phone: {r.Payee.Phone}</p>   
+//         //         <p>Address: {r.Payee.Address.Address1}</p> 
+//         //         <p>City: {r.Payee.Address.City}</p> 
+//         //         <p>State or Province: {r.Payee.Address.StateOrProvince}</p> 
+//         //         <p>Country: {r.Payee.Address.Country}</p> 
+//         //         <p>Postal Code: {r.Payee.Address.PostalCode}</p> 
+//         //         <p>Attention: {r.Payee.Attention}</p> 
+//         //         <p>Submission Date: {r.Payee.SubmissionDate}</p> 
+//         //         <br/> 
+//         //         <h2> Payment</h2>
+//         //         <p>PAN: {r.Payment.PAN}</p>
+//         //         <p>CVV: {r.Payment.CVV}</p>
+//         //         <p>Exp: {r.Payment.Exp}</p>
+                
+//         //         </div>
+//         //     )
+        
+//         // })
         
 
-        const test = this.props.lion
-        const testing = this.props.tiger
+//         // const test = this.props.lion
+//         // const testing = this.props.tiger
+//         // const ew3a = this.props.bear
       
         
 
-        console.log(test)
-        console.log(testing)
-        console.log(this.state.file)
-        console.log(displayR)
-     
+//         // console.log(test)
+//         // console.log(testing)
+//         // console.log(this.state.file)
+//         // console.log(displayR)   
+
+      
+
+
+//             // return (
+//             // <div>
+//             //     {/* <p>paymmmmeentss</p>
+//             //     <button onClick={(e) => this.handleBlack(displayR, e)}> Get Client File</button>
+//             //     <p>customer name: {test}</p>
+//             //     <p>wow: {ew3a}</p>
+//             //     <p>Test: {displayR}</p>
+//             // */}
 
 
 
-            return (
-            <div>
-                <p>paymmmmeentss</p>
-                <button onClick={(e) => this.handleBlack(displayR, e)}> Get Client File</button>
-                <p>customer name: {test}</p>
-                <p>Payment PAN: {displayR}</p>
-              
 
 
-            </div>
-        )
-    }
-}
+
+//         // </div>
+
+//     }
+// }
 
 
-export default Payment
+// export default Payment
