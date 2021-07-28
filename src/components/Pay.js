de073675551d7afede0a13505b7f300008286b6c
Ahmed Elnasseh <elnassehag@vcu.edu>
2021-07-27 23:57:16 -0400

    finale

--- src/components/Pay.js
+++ src/components/Pay.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import './Pay.css'
+import '../index.css'
 
 
 class Pay extends React.Component {
@@ -9,57 +11,66 @@ class Pay extends React.Component {
         currentClientIndex: 0
     };
 
+    //function that changes the state to the current client based on index clicked
     handleButtonClick(index) {
-        console.log('button clicked!', { index })
         this.setState({ currentClientIndex: index })
     }
 
+    //function that creates the buttons of clients to select from 
     renderButtons() {
         return this.props.pay.map((p, index) => (
-            <button key={index} onClick={() => this.handleButtonClick(index)}>
+            <button className="button" key={index} onClick={() => this.handleButtonClick(index)}>
                 {p.Payee.Name}
             </button>
         ))
     }
 
+    //function that renders the client, payment, and remittance information of the selected client
     renderContent() {
         const currentPay = this.props.pay[this.state.currentClientIndex]
         return (
-            <div className='content'>
+            <div className="content">
+                <div className='payee'>
                 <h2>Payee</h2>
-                <p>Name: {currentPay.Payee.Name}</p><br />
-                <p>Fax: {currentPay.Payee.Fax}</p><br />
-                <p>Phone: {currentPay.Payee.Phone}</p><br />
-                <p>Address: {currentPay.Payee.Address.Address1}</p><br />
-                <p>City: {currentPay.Payee.Address.City}</p><br />
-                <p> State or Province: {currentPay.Payee.Address.StateOrProvince}</p><br />
-                <p> Country: {currentPay.Payee.Address.Country}</p><br />
-                <p>PostalCode: {currentPay.Payee.Address.PostalCode}</p><br />
+                <p><b>Name:</b> {currentPay.Payee.Name}</p>
+                <p><b>Fax:</b> {currentPay.Payee.Fax}</p>
+                <p><b>Phone:</b> {currentPay.Payee.Phone}</p>
+                <p><b>Address:</b> {currentPay.Payee.Address.Address1}</p>
+                <p><b>City:</b> {currentPay.Payee.Address.City}</p>
+                <p> <b>State or Province:</b> {currentPay.Payee.Address.StateOrProvince}</p>
+                <p> <b>Country:</b> {currentPay.Payee.Address.Country}</p>
+                <p><b>PostalCode:</b> {currentPay.Payee.Address.PostalCode}</p>
                 <br />
-                <p>Attention: {currentPay.Payee.Attention}</p><br />
-                <p>Submission Date: {currentPay.Payee.SubmissionDate}</p><br />
-                <br />
+                <p><b>Attention:</b> {currentPay.Payee.Attention}</p>
+                <p><b>Submission Date:</b>{currentPay.Payee.SubmissionDate}</p>
+                
+                </div>
+                
+                <div className='payment'>
+                <h2><b>Payment</b></h2>
+                <p><b>PAN:</b> {currentPay.Payment.PAN}</p>
+                <p><b>CVV: </b>{currentPay.Payment.CVV}</p>
+                <p><b> Exp: </b>{currentPay.Payment.Exp}</p>
+                
+                </div>
 
-                <h2>Payment</h2>
-                <p>PAN: {currentPay.Payment.PAN}</p><br />
-                <p>CVV: {currentPay.Payment.CVV}</p><br />
-                <p> Exp: {currentPay.Payment.Exp}</p><br />
-
+                <div className='remittance'>
                 <h2>Remittance</h2>
                 <ul key={currentPay.Payee.Name}>
                     {currentPay.Remittance.map(remit => {
                         return (
                             <ul key={remit.PayorName}>
-                                <li key={remit.PayorId}>Payor Name: {remit.PayorName}</li>
-                                <li>Payor Id: {remit.PayorId}</li>
-                                <li>Invoice No: {remit.InvoiceNo}</li>
-                                <li>Descriptiom: {remit.Description}</li>
-                                <li>Amount: {remit.Amount}</li>
-                                <br />
+                                <li key={remit.PayorId}><b>Payor Name: </b>{remit.PayorName}</li>
+                                <li><b>Payor Id:</b> {remit.PayorId}</li>
+                                <li><b>Invoice No:</b> {remit.InvoiceNo}</li>
+                                <li><b>Description:</b> {remit.Description}</li>
+                                <li><b>Amount: </b>{remit.Amount}</li>
+                                <br/>
                             </ul>
                         )
                     })}
                 </ul>
+                </div>
 
             </div>
         )
